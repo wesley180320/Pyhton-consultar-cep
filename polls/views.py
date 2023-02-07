119c53c77bbd7164ada9fbbef12e365e7c720ff3
wesley180320 <wesleyestevao333@gmail.com>
2023-02-06 22:39:41 -0400

    Projeto finalizado

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,27 @@
+from urllib import request
+
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
+
+from . import models
 
 
 def index(request):
+
+
+    if request.method == "POST":
+        cep = request.POST.get('cep', None)
+        requisicao = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
+        dados2 = requisicao.json()
+
+        print(dados2['cep'])
+
+        endereco = models.dados()
+        endereco.localidade = dados2['localidade']
+        endereco.rua = dados2['logradouro']
+        endereco.bairro = dados2['bairro']
+        endereco.save()
 
     return render(request, 'polls/index.html')
 
